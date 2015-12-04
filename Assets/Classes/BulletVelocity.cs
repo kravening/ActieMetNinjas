13071da207e566b454cb912dd24a31c6316fb2e8
kerim birlik <kerim.birlik@gmail.com>
2015-12-04 20:00:12 +0100

    bullet transform fix

--- Assets/Classes/BulletVelocity.cs
+++ Assets/Classes/BulletVelocity.cs
@@ -4,13 +4,7 @@ using System.Collections;
 public class BulletVelocity : MonoBehaviour {
     [SerializeField]private float speed;
 
-    private Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-
-    }
 	void FixedUpdate () {
-        rb.AddForce(transform.right * speed, ForceMode.Force);
+        transform.position += transform.right * speed;
     }
 }
