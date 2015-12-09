df3573afcbad2cb2c7a6972f9fa5a20bdbbbfd12
Kerim Birlik <kerim.birlik@gmail.com>
2015-12-09 16:44:51 +0100

    added photon connect doesnt work correctly yet.

---
+++ Assets/MpOnConnect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MpOnConnect : MonoBehaviour {
+
+	private Vector3 position;
+	void OnJoinedRoom()
+	{
+		CreatePlayerObject();
+	}
+
+	void CreatePlayerObject()
+	{
+		position = Vector3.zero;
+		GameObject newPlayerObject = PhotonNetwork.Instantiate("PlayerTank", position, Quaternion.identity, 0);
+		
+	}
+}
--- Assets/Scripts/Player/KeyboardMovement.cs
+++ Assets/Scripts/Player/KeyboardMovement.cs
@@ -7,21 +7,32 @@ public class KeyboardMovement : MonoBehaviour {
 	private CharacterController characterController;
 	[SerializeField]
 	private float currentMovementSpeed = 5f;
+
+	//multiplayervars
+	PhotonView m_PhotonView;
+	PhotonTransformView m_TransformView;
+
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
+		m_PhotonView = GetComponent<PhotonView>();
+		m_TransformView = GetComponent<PhotonTransformView>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+		if (m_PhotonView.isMine == true)
+		{
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
 
-		if (moveHorizontal > 0f || moveHorizontal < 0f || moveVertical > 0f || moveVertical < 0f) {
-			moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
-			moveDirection = transform.TransformDirection(moveDirection);
-		
-		characterController.Move(moveDirection * currentMovementSpeed * Time.deltaTime);
+			if (moveHorizontal > 0f || moveHorizontal < 0f || moveVertical > 0f || moveVertical < 0f)
+			{
+				moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+				moveDirection = transform.TransformDirection(moveDirection);
+
+				characterController.Move(moveDirection * currentMovementSpeed * Time.deltaTime);
+			}
 		}
 	}
 }
